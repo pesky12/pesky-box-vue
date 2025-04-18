801ab67ddbb5e846bfdabeaa3e153fc21f4ae338
Pesky <avianearts@gmail.com>
2025-04-18 20:01:51 +0200

    Fix publicPath to use lowercase repository name for production builds

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,6 @@ module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false, // Prevents ESLint errors from failing the build
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/' + (process.env.REPOSITORY_NAME || 'PeskyBoxVue') + '/'
+    ? '/' + (process.env.REPOSITORY_NAME || 'pesky-box-vue') + '/'
     : '/'
 })
